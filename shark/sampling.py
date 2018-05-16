V0.3.0 Added functionality to monitor convergence and add samples sequentially - see example 2 dmp.py - freezeThawCyclesDTU(...) * Error fix in calculation of moist freeze-thaw cycles

shark/sampling.py
@@ -21,7 +21,9 @@ def sobol(m, dim, sets=1):
         design = np.vstack((design,d))
     return design
 
-def main(scenario, dist, runs, sets, strat, path):
+def main(scenario, dist, runs, sets, strat, path, seq=None):
+    if not os.path.exists(path):
+        os.mkdir(path)
     if strat == 'load':
         # load file
         file = os.path.join(path,'samples_raw.mat')
@@ -33,6 +35,13 @@ def main(scenario, dist, runs, sets, strat, path):
         # create raw sampling scheme
         if strat == 'sobol':
             samples_raw = sobol(m=runs, dim=dist.shape[0],sets=sets)
+        elif strat == 'sobol convergence':
+            try:
+                samples_raw = np.load(os.path.join(path,'samples_'+str(seq)+'.npy'))
+            except FileNotFoundError:
+                samples_raw = sobol(m=2**12, dim=dist.shape[0],sets=1)
+                np.save(os.path.join(path,'samples_'+str(seq)), samples_raw)
+            samples_raw = samples_raw[0:runs,:]
         else:
             print('''Error: This sampling strategy is not supperted. Currently only 'sobol' and 'load' are implemented.''')
     # Add keys to dictionary
@@ -45,10 +54,10 @@ def main(scenario, dist, runs, sets, strat, path):
         sdf = pd.DataFrame({})
         sdf[scenario['parameter']] = [s]*samples_raw.shape[0]
         for i in range(dist.shape[0]):
-            dist_type = dist.get_value(i, 'type')
+            dist_type = dist.at[i, 'type']
             x = samples_raw[:,i]
             if dist_type == 'discrete':
-                p1 = dist.get_value(i, 'value')
+                p1 = dist.at[i, 'value']
                 if isinstance(p1, int): 
                     high = p1+1
                     values = randint.ppf(x, low=1, high=high).tolist()
@@ -56,22 +65,26 @@ def main(scenario, dist, runs, sets, strat, path):
                     high = len(p1)
                     values = randint.ppf(x, low=0, high=high).tolist()
                     values = [p1[int(x)] for x in values]
-                sdf[dist.get_value(i, 'parameter')] = values
+                sdf[dist.at[i, 'parameter']] = values
             elif dist_type == 'uniform':
-                p1 = dist.get_value(i, 'value')[0]
-                p2 = dist.get_value(i, 'value')[1]
+                p1 = dist.at[i, 'value'][0]
+                p2 = dist.at[i, 'value'][1]
                 values = uniform.ppf(x, loc=p1, scale=p2-p1).tolist()
-                sdf[dist.get_value(i, 'parameter')] = values
+                sdf[dist.at[i, 'parameter']] = values
             elif dist_type == 'normal':
-                p1 = dist.get_value(i, 'value')[0]
-                p2 = dist.get_value(i, 'value')[1]
+                p1 = dist.at[i, 'value'][0]
+                p2 = dist.at[i, 'value'][1]
                 values = norm.ppf(x, loc=p1, scale=p2).tolist()
-                sdf[dist.get_value(i, 'parameter')] = values
+                sdf[dist.at[i, 'parameter']] = values
             else:
                 print('ERROR: distribution type not supported')
                 sys.exit()
         samples = samples.append(sdf, ignore_index=True)
     # Save samples
-    samples.to_excel(os.path.join(path,'samples.xlsx'))
-    samples.to_pickle(os.path.join(path,'samples'))
+    if seq == None:
+        name = 'samples'
+    else:
+        name = 'samples_'+str(seq)
+    samples.to_excel(os.path.join(path, name+'.xlsx'))
+    samples.to_pickle(os.path.join(path,name))
     return samples

shark/supp.py
@@ -489,11 +489,10 @@ def nameccd(climateparam):
 
 def readData(args):
     n, files_num = args
-    print(n)
     output_fn, geometry_fn, elements_fn = dict(), dict(), dict()
     geom_x, geom_y = None, None
     for file in files_num:
-        p = re.sub('[0-9_]', '', file.split('\\')[-1][:-4])
+        p = re.sub('[0-9_-]', '', file.split('\\')[-1][:-4])
         with open(file, 'r', encoding='utf8') as f:
             l = 0
             for line in f:
@@ -503,7 +502,7 @@ def readData(args):
                     geom_y = string2vec(f.readline())
                     l += 2
                 # Find output start line
-                if 'ELEMENTS' in line:
+                if 'ELEMENTS' in line or 'SIDES 'in line:
                     elem_f = string2vec(line)
                     output_f = np.loadtxt(file,skiprows=l+1,usecols=tuple(range(1,len(elem_f)+1)),dtype='f')
                     break
@@ -514,23 +513,29 @@ def readData(args):
             output_fn[p] = output_f
     return output_fn, geometry_fn, elements_fn
 
-def readOutput(path):
+def readOutput(path, exclude):
     # Get list of all files that need to be read
-    files = getFileList(path, ext='.out')[1]
+    files_all = getFileList(path, ext='.out')[1]
+    files = list()
+    for f in files_all:
+        if not any(e in f for e in exclude):
+            files.append(f)
     # Extract output parameters from list
-    param = list(Counter([re.sub('[0-9_]', '', x.split('\\')[-1][:-4]) for x in files]).keys())
+    param = list(Counter([re.sub('[0-9_-]', '', x.split('\\')[-1][:-4]) for x in files]).keys())
     # Extract numbers from list
     num = list(Counter([re.sub('[a-zA-Z]', '', x[:-4]).split('_')[-1] for x in files]).keys())
+    do = [[int(x.split('-')[0]) for x in num], [int(x.split('-')[1]) for x in num]]
+    tuples = list(zip(*do))
     # Read files
-    args = [(n, [x for x in files if re.sub('[a-zA-Z]', '', x[:-4]).split('_')[-1] == n]) for n in num]
     num_cores = multiprocessing.cpu_count()-1
     pool = multiprocessing.Pool(num_cores)
+    args = [(n, [x for x in files if re.sub('[a-zA-Z]', '', x[:-4]).split('_')[-1] == n]) for n in num]
     results = pool.map(readData, args)
     pool.close()
     pool.join()
     
-    output = pd.DataFrame([x[0] for x in results], columns=param, index=num)
-    geometry = pd.DataFrame([x[1] for x in results], columns=param, index=num)
-    elements = pd.DataFrame([x[2] for x in results], columns=param, index=num)
+    output = pd.DataFrame([x[0] for x in results], columns=param, index=pd.MultiIndex.from_tuples(tuples))
+    geometry = pd.DataFrame([x[1] for x in results], columns=param, index=pd.MultiIndex.from_tuples(tuples))
+    elements = pd.DataFrame([x[2] for x in results], columns=param, index=pd.MultiIndex.from_tuples(tuples))
     
     return output, geometry, elements